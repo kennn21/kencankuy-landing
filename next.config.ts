cd8f49173cfc282d37463371bd1abc04123333fe
FX Kennard Sugirotok <kennard.sugirotok@axcel.partners>
2025-08-27 14:08:17 +0700

    chore: add webpack as a dependency and update typescript version in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: Configuration) => {
+    // Exclude source maps from being processed by loaders
+    config.module?.rules?.push({
+      test: /\.js\.map$/,
+      loader: "ignore-loader",
+    });
+
+    return config;
+  },
+
+  // ... your other Next.js configurations
 };
 
 export default nextConfig;
