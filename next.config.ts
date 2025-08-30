f1fde5e8bdd8632533843b414b1d8129e8e5e541
FX Kennard Sugirotok <kennard.sugirotok@axcel.partners>
2025-08-30 13:23:47 +0700

    fix: bring back webpack

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config: Configuration) => {
+    // Exclude source maps from being processed by loaders
+    config.module?.rules?.push({
+      test: /\.js\.map$/,
+      loader: "ignore-loader",
+    });
+
+    return config;
+  },
+
+  // ... your other Next.js configurations
+};
 
 export default nextConfig;
