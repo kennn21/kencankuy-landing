f70a950a8025f105da019160bfb2e7ec1fa3a46e
FX Kennard Sugirotok <kennard.sugirotok@axcel.partners>
2025-08-31 14:55:22 +0700

    chore: icon

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedRoutes = ["/settings"];
+const protectedRoutes = ["/settings", "/admin"];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
